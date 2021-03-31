09f0c62ec52cb533f741047fdc088fa423e8f1e5
Ava DeLaCruz <avadelac@usc.edu>
2021-03-31 13:47:32 -0700

    add club 101 page

--- src/components/sections/GuideExpanded2.js
+++ src/components/sections/GuideExpanded2.js
@@ -58,15 +58,74 @@ const GuideExpanded2 = ({
 						>
 							<div className="tiles-item-inner">
 								<div className="testimonial-item-content">
-									<p className="text-sm mb-0">— [guide here]</p>
+									<h3>College Clubs and Activities 101</h3>
+									<p className="text-sm mb-0">
+										— In my opinion, one of the best parts about my college
+										experience has been the clubs and extracurricular activites.
+										Clubs and extracurriculars in college are pretty different
+										from high school, although there are some similarities.
+									</p>{" "}
+									<br></br>
+									<p className="text-sm mb-0">
+										Personally, I love coding. At my high school, we had one
+										Computer-Science-related-club, which was focused on teaching
+										young girls to code. Even though I loved this experience,
+										there weren't any opportunities to learn how to code
+										different things myself, like apps or websites, or to
+										explore different areas of Computer Science, like
+										cybersecurity or artificial intelligence.
+									</p>
+									<br></br>
+									<p className="text-sm mb-0">
+										In college, this is totally different. Colleges are usually
+										bigger than high schools and have more students with more
+										interests, and so there are more clubs to choose from. For
+										example, at USC, we have a cyber security club called
+										CybOrg, an artificial intelligence club called CAIS++, a
+										coding for social good club called Code the Change, and the
+										list goes on and on.
+									</p>
+									<br></br>
+									<p className="text-sm mb-0">
+										If tech isn't your thing, no worries :,) There are so many
+										types of different clubs at college, and Computer-Science
+										clubs are just a small example. There are a few main groups
+										of clubs, like...
+									</p>
+									<br></br>
+									<ul>
+										<li>
+											Club Sports, such as soccer, flag football, and tennis
+										</li>
+										<li>
+											Cultural Organizations, like Latinx Student Assembly or la
+											CASA (Latinx Chicanx Center for Advocacy and Student
+											Affairs)
+										</li>
+										<li>
+											Recreational/Hobby Clubs, like Art on the Grass and Board
+											Game club
+										</li>
+										<li>Service and Volunteering clubs, and</li>
+										<li>Spiritual + Wellness clubs</li>
+									</ul>
+									<br></br>
+									<p className="text-sm mb-0">
+										Although these vary from school to school, most schools will
+										have clubs in each category. For more examples, you can
+										explore USC's student organization page here:{" "}
+										<a href="https://usc.campuslabs.com/engage/organizations">
+											https://usc.campuslabs.com/engage/organizations.
+										</a>
+									</p>
 								</div>
 								<div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
 									<span className="testimonial-item-name text-color-high">
-										[author text here if u want]
+										written by Ava DeLaCruz
 									</span>
 									<span className="text-color-low"> / </span>
 									<span className="testimonial-item-link">
-										<a href="#0">[author details]</a>
+										<a href="#0">Computer Science student, Class of 2022</a>
 									</span>
 								</div>
 							</div>
