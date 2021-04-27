5ff3a5d235b190a57811ef4be3b27d0ba250b75e
Ava DeLaCruz <avadelac@usc.edu>
2021-04-27 14:47:18 -0700

    Updates

---
+++ precache-manifest.ba0f88451a2d6db5ffd00e2996c8cd7a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41cfb034044d6a7c5e1496a129428c1",
+    "url": "/499-project/index.html"
+  },
+  {
+    "revision": "eeb3ec9046691459793a",
+    "url": "/499-project/static/css/main.b4c9df32.chunk.css"
+  },
+  {
+    "revision": "af4eccb6af79c6d2c2ad",
+    "url": "/499-project/static/js/2.15a36d81.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/499-project/static/js/2.15a36d81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb3ec9046691459793a",
+    "url": "/499-project/static/js/main.1da780be.chunk.js"
+  },
+  {
+    "revision": "25a57d562686df43cdf8",
+    "url": "/499-project/static/js/runtime-main.41a8d406.js"
+  },
+  {
+    "revision": "0cb73257f368d30490ea03ad07bef869",
+    "url": "/499-project/static/media/computer.0cb73257.svg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/499-project/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/499-project/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/499-project/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/499-project/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/499-project/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/499-project/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/499-project/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "5a1faf915b816d97775d5c1523aaf4f1",
+    "url": "/499-project/static/media/frankie.5a1faf91.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/499-project/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/499-project/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "a1a2f04e4ba47270e67e9208072a068a",
+    "url": "/499-project/static/media/lina.a1a2f04e.jpeg"
+  },
+  {
+    "revision": "43f20f874c510a31be60eaac0e21b0e3",
+    "url": "/499-project/static/media/logo.43f20f87.svg"
+  },
+  {
+    "revision": "ab26190fb348911ca06a3b678779961e",
+    "url": "/499-project/static/media/luis.ab26190f.jpg"
+  },
+  {
+    "revision": "fb6d3d2f4789c11701773b2fc2a60cd9",
+    "url": "/499-project/static/media/student-smiling.fb6d3d2f.png"
+  },
+  {
+    "revision": "6360e77fe4972198f5e55db066a9f892",
+    "url": "/499-project/static/media/students.6360e77f.svg"
+  }
+]);
