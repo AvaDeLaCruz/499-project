f0298de41dfe8d9b9439f2ad8205bb0e8143d8de
Yash Chandak <ychandak@usc.edu>
2021-04-28 13:08:40 -0500

    integrated formatted CTA with email API

--- src/components/sections/Email.js
+++ src/components/sections/Email.js
@@ -15,7 +15,33 @@ const defaultProps = {
   split: false
 }
 
-export default function Email() {
+const Email = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  split,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'cta section center-content-mobile reveal-from-bottom',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'cta-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider',
+    split && 'cta-split'
+  );  
 
   function sendEmail(e) {
     e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
@@ -27,84 +53,35 @@ export default function Email() {
           console.log(error.text);
       });
   }
-
+  
   return (
-    <form className="contact-form" onSubmit={sendEmail}>
-      <input type="hidden" name="contact_number" />
-      <label>Name</label>
-      <input type="text" name="from_name" />
-      <label>Email</label>
-      <input type="email" name="to_email" />
-      <label>Subject</label>
-      <input type="text" name="subject" />
-      <label>Message</label>
-      <textarea name="html_message" />
-      <input type="submit" value="Send" />
-    </form>
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <div className="cta-slogan">
+            <h3 className="m-0">
+              Want an email reminder to come back to our site?
+            </h3>
+          </div>
+          <div className="cta-action">
+            <form className="contact-form" onSubmit={sendEmail}>
+              <Input id="newsletter" type="email" name="to_email" hasIcon="right" placeholder="Your best email" required>
+                <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
+                </svg>
+              </Input>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
-// const Email = ({
-//   className,
-//   topOuterDivider,
-//   bottomOuterDivider,
-//   topDivider,
-//   bottomDivider,
-//   hasBgColor,
-//   invertColor,
-//   split,
-//   ...props
-// }) => {
+Email.propTypes = propTypes;
+Email.defaultProps = defaultProps;
 
-//   const outerClasses = classNames(
-//     'cta section center-content-mobile reveal-from-bottom',
-//     topOuterDivider && 'has-top-divider',
-//     bottomOuterDivider && 'has-bottom-divider',
-//     hasBgColor && 'has-bg-color',
-//     invertColor && 'invert-color',
-//     className
-//   );
-
-//   const innerClasses = classNames(
-//     'cta-inner section-inner',
-//     topDivider && 'has-top-divider',
-//     bottomDivider && 'has-bottom-divider',
-//     split && 'cta-split'
-//   );  
-
-  
-//   return (
-//     <section
-//       {...props}
-//       className={outerClasses}
-//     >
-//       <div className="container">
-//         <div
-//           className={innerClasses}
-//         >
-//           <div className="cta-slogan">
-//             <h3 className="m-0">
-//               Want an email reminder to come back to our site?
-//             </h3>
-//           </div>
-//           <div className="cta-action">
-//             <form>
-//               <Input id="newsletter" type="email" hasIcon="right" placeholder="Your best email" required>
-//                 <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-//                   <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-//                 </svg>
-//               </Input>
-//               <Input type="button" value="Submit" className="button button-primary button-wide-mobile button-sm" onClick={handleSubmit} />
-//             </form>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
-
-// Email.propTypes = propTypes;
-// Email.defaultProps = defaultProps;
-
-// export default Email;
+export default Email;
